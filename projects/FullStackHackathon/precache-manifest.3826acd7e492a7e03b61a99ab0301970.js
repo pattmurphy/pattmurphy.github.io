e0d1d06f0a66d0abebe2298f5ac957836d7ce6ad
pattmurphy <43220681+pattmurphy@users.noreply.github.com>
2022-01-14 02:01:48 -0500

    security stuff

---
+++ projects/FullStackHackathon/precache-manifest.3826acd7e492a7e03b61a99ab0301970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55da60f41ff7a198b280",
+    "url": "./static/css/main.b8edf55b.chunk.css"
+  },
+  {
+    "revision": "55da60f41ff7a198b280",
+    "url": "./static/js/main.7b218452.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "228cac418cdcc544c324",
+    "url": "./static/css/2.4f854e56.chunk.css"
+  },
+  {
+    "revision": "228cac418cdcc544c324",
+    "url": "./static/js/2.3177bda1.chunk.js"
+  },
+  {
+    "revision": "fee8d6abe37446469de46a437f450c35",
+    "url": "./index.html"
+  }
+];
---
+++ projects/FullStackHackathon/precache-manifest.b961d57f8764ddb08f8637c051bcff19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d35132ebf49bd2338ee3",
+    "url": "./static/css/main.b8edf55b.chunk.css"
+  },
+  {
+    "revision": "d35132ebf49bd2338ee3",
+    "url": "./static/js/main.cbe05834.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c27dfbde0018d14e51da",
+    "url": "./static/css/2.e11499a4.chunk.css"
+  },
+  {
+    "revision": "c27dfbde0018d14e51da",
+    "url": "./static/js/2.08726898.chunk.js"
+  },
+  {
+    "revision": "b8325e5a00067eea36ae1a2c406d9dcf",
+    "url": "./index.html"
+  }
+];
